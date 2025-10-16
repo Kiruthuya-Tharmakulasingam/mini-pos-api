815f9a324e269c0e4913b86cdadc8a74e5286160
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-16 19:17:47 +0530

    database connected

---
+++ app.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// db connection
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log("DB Connected Successfully!"));
+  } catch (error) {
+    console.error("DB Connection Error:", error.message);
+  }
+};
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
