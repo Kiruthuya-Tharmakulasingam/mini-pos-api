d29ecae27c8a99bd06b1b8a000276dcaee756a63
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-16 21:46:43 +0530

    item method , schema and router created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
 import customerRoutes from "./routes/customerRoutes.js";
+import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 connectDB();
 //Use Routes
 app.use("/api/customers", customerRoutes);
+app.use("/api/items", itemRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
